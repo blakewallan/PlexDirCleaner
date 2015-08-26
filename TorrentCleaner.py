8b0a1a4a537f4e28b21c1ea8e6ff40fc34b3d0c3
Blake Allan <blakewallan@gmail.com>
2015-08-25 20:45:23 -0700

    Fixed the shouldDelete message box text, wasnt using proper string concatination

--- TorrentCleaner.py
+++ TorrentCleaner.py
@@ -18,60 +18,56 @@ filepath = filedialog.askdirectory()
 
 print("\nScanning: ", filepath, "\n")
 
-def showScan():
+# variables to keep track of file sizes, directory sizes, and size of files to be deleted
+totalScanned = 0
+totalDeleted = 0
+badFileCount = 1
 
-    #variables to keep track of file sizes, directory sizes, and size of files to be deleted
-    totalScanned = 0
-    totalDeleted = 0
-    badFileCount = 1
+for root, dirs, files in os.walk(filepath):
+    for file in files:
+        totalScanned += 1
+        fileSize = (os.path.join(root, file))
+        if file.endswith(badExtension) or (
+                        (os.path.getsize(fileSize) / 1048576) < 20 and (os.path.getsize(fileSize) / 1048576) > 0.1):
 
-    for root, dirs, files in os.walk(filepath):
-        for file in files:
-            totalScanned += 1
-            fileSize = (os.path.join(root, file))
-            if file.endswith(badExtension) or ((os.path.getsize(fileSize) / 1048576) < 20 and (os.path.getsize(fileSize) / 1048576) > 0.1) :
+            badFile = (os.path.join(root, file))
 
-                badFile = (os.path.join(root, file))
+            deletedfileSize = os.path.getsize(badFile)
 
-                deletedfileSize = os.path.getsize(badFile)
-                
-                megabytes = round(deletedfileSize / 1048576, 2)
-                gigabytes = round(deletedfileSize / 1073741824, 2)
+            megabytes = round(deletedfileSize / 1048576, 2)
+            gigabytes = round(deletedfileSize / 1073741824, 2)
 
-                print("Found: ", file)
-                #Case to print file size in Gb or Mb depending on size
-                if gigabytes > 1:
-                    print("File Size = ", gigabytes, "Gb\n")
-                else:
-                    print("File Size = ", megabytes, "Mb\n")
+            print("Found: ", file)
+            # Case to print file size in Gb or Mb depending on size
+            if gigabytes > 1:
+                print("File Size = ", gigabytes, "Gb\n")
+            else:
+                print("File Size = ", megabytes, "Mb\n")
 
-                # Update the total size of bad files
-                totalDeleted += deletedfileSize
-                badFileCount += 1
-
-                return totalScanned, totalDeleted, badFileCount
+            # Update the total size of bad files
+            totalDeleted += deletedfileSize
+            badFileCount += 1
 
 print("\nScan Complete")
 
-def totalFound():
-    totalDeleted = showScan()[1]
-    if round(totalDeleted / 1073741824, 2) > 1:
-        totalGbDeleted = round(totalDeleted / 1073741824, 2)
-        return "Found ", showScan()[2], " bad files\nTotal storage to cleared: ", totalGbDeleted, "Gb"
+if round(totalDeleted / 1073741824, 2) > 1:
+    totalGbDeleted = round(totalDeleted / 1073741824, 2)
+    message = "Found " + str(badFileCount) + " bad files\nTotal storage to cleared: "+ str(totalGbDeleted) + "Gb"
 
-    else:
-        round(totalDeleted / 1048576, 2)
-        totalMbDeleted = round(totalDeleted / 1048576, 2)
-        message = "Found ", showScan()[2], " bad files\nTotal storage to cleared: ", totalMbDeleted, "Mb"
-        return message
+else:
+    round(totalDeleted / 1048576, 2)
+    totalMbDeleted = round(totalDeleted / 1048576, 2)
+    message = "Found " + str(badFileCount) + " bad files\nTotal storage to cleared: " + str(totalMbDeleted) + "Mb"
 
-shouldDelete = messagebox.askquestion("Delete?",  totalFound())
+shouldDelete = messagebox.askquestion("Delete?", message)
+print(message)
 
 if shouldDelete == "yes":
     for root, dirs, files in os.walk(filepath):
         for file in files:
             fileSize = (os.path.join(root, file))
-            if file.endswith(badExtension) or ((os.path.getsize(fileSize) / 1048576) < 20 and (os.path.getsize(fileSize) / 1048576) > 0.1) :
+            if file.endswith(badExtension) or (
+                            (os.path.getsize(fileSize) / 1048576) < 20 and (os.path.getsize(fileSize) / 1048576) > 0.1):
                 badFile = (os.path.join(root, file))
                 send2trash.send2trash(badFile)
 
